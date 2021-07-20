cbd062b1e16c49fdf2383ca59f883a99c54b8328
ryannbajaj <82589750+ryannbajaj@users.noreply.github.com>
2021-07-20 19:53:36 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,25 @@
+class Ball{
+    constructor(x, y) {
+      var options = {
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+      }
+      this.body = Bodies.circle(x, y, 15, options);
+      this.width = 15;
+      this.height = 15;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(RADIUS);
+      fill("red");
+      ellipse(0, 0, this.width, this.height);
+      pop();
+    }
+  };
